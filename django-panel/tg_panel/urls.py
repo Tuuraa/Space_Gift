b8ddec55b38fe4289b621acf0bede82127871f85
kit <emeralddog@list.ru>
2023-01-16 20:12:41 +0400

    fixes

--- django-panel/tg_panel/urls.py
+++ django-panel/tg_panel/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .views import index
-
+from .views import index, deposit_transfer
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('api_v1/deposit_transfer', deposit_transfer)
 ]
