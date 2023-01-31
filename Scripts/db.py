e91d34ff538833045bb88f9b11843406ca9573af
kit <emeralddog@list.ru>
2023-01-31 20:45:49 +0700

    reactivate

--- Scripts/db.py
+++ Scripts/db.py
@@ -1798,7 +1798,7 @@ class ManagerResetSystem:
         connection, cursor = await async_connect_to_mysql(loop)
         async with connection.cursor() as cursor:
             await cursor.execute("""
-            UPDATE `users` SET `is_include_ref` = '0', `reinvest` = '0', `percent_ref_money` = '0',
+            UPDATE `users` SET `is_include_ref` = '0',
             `archive_dep` = `archive_dep` + `amount_gift_money`, `planet` = '0', `status` = '0',
             `step` = '0', `activate_date` = NULL, `count_ref` = '0', `activate_ref_count` = '0'
             """)
